dfb4be4c1381c78aa1e7ce27c66657cb095b8cca
karma-yrb <yrbpro@gmail.com>
2025-05-24 18:55:51 +0200

    Harmonisation du markup du formulaire utilisateur (EditUser) :\n- Suppression de la div superflue et des classes redondantes.\n- Structure plus propre : <form class="uni-form-container"><div class="uni-form">...</div></form>

--- src/components/users/EditUser.js
+++ src/components/users/EditUser.js
@@ -63,13 +63,15 @@ function EditUser() {
         <i className="fa fa-user"></i>
         {id ? 'Modifier' : 'Ajouter'} un utilisateur
       </h1>
-      <form className="uni-form" onSubmit={handleSubmit}>
-        <UserForm
-          formData={user}
-          handleChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
-          id={id}
-        />
-        <button type="submit">{id ? 'Mettre à jour' : 'Créer'}</button>
+      <form className="uni-form-container" onSubmit={handleSubmit}>
+        <div className="uni-form">
+          <UserForm
+            formData={user}
+            handleChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
+            id={id}
+          />
+          <button type="submit">{id ? 'Mettre à jour' : 'Créer'}</button>
+        </div>
       </form>
     </div>
   );
